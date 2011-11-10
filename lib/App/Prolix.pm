ffeca23c7c9ba160028fd6573d7d8d8243eb316e
Gaal Yahas <gaal@forum2.org>
2011-11-11 00:37:39 +0200

    This be App::Prolix, 0.01.

--- lib/App/Prolix.pm
+++ lib/App/Prolix.pm
@@ -13,7 +13,7 @@ use v5.10;
 package App::Prolix::ConfigFileRole;
 
 use Moose::Role;
-with 'MooseX::ConfigFromFile';
+with "MooseX::ConfigFromFile";
 use JSON 2.0;
 
 sub get_config_from_file {
@@ -27,50 +27,56 @@ sub get_config_from_file {
 
 }
 
+use Data::Munge;
+use IO::File;
 use IPC::Run ();
 use Term::ReadKey ();
 use Term::ReadLine;
+use Text::Balanced ();
 use Try::Tiny;
-use IO::File;
 
 use App::Prolix::MooseHelpers;
 
-with 'App::Prolix::ConfigFileRole';
-with 'MooseX::Getopt';
+with "App::Prolix::ConfigFileRole";
+with "MooseX::Getopt";
 
 # Flags affecting overall run style.
-has_option 'verbose' => (isa => 'Bool', cmd_aliases => 'v',
-    documentation => 'Prints extra information.');
-has_option 'pipe' => (isa => 'Bool', cmd_aliases => 'p',
-    documentation => 'Reads from stdin instead of interactively.');
-has_option 'log' => (isa => 'Str', cmd_aliases => 'l');
+has_option "verbose" => (isa => "Bool", cmd_aliases => "v",
+    documentation => "Prints extra information.");
+has_option "pipe" => (isa => "Bool", cmd_aliases => "p",
+    documentation => "Reads from stdin instead of interactively.");
+has_option "log" => (isa => "Str", cmd_aliases => "l");
 
 # Flags affecting filtering.
-has_option 'ignore_re' => (isa => 'ArrayRef', cmd_aliases => 'r',
-    'default' => sub { [] },
-    documentation => 'Ignore lines matching this regexp.');
-has_option 'ignore_line' => (isa => 'ArrayRef', cmd_aliases => 'i',
-    'default' => sub { [] },
-    documentation => 'Ignore lines exactly matching this.');
-has_option 'ignore_substring' => (isa => 'ArrayRef', cmd_aliases => 'b',
-    'default' => sub { [] },
-    documentation => 'Ignore lines containing this substring.');
-has_option 'snippet' => (isa => 'ArrayRef', cmd_aliases => 's',
-    'default' => sub { [] },
-    documentation => 'Snip lines. Use s/search_re/replace/ syntax.');
+has_option "ignore_re" => (isa => "ArrayRef", cmd_aliases => "r",
+    "default" => sub { [] },
+    documentation => "Ignore lines matching this regexp.");
+has_option "ignore_line" => (isa => "ArrayRef", cmd_aliases => "i",
+    "default" => sub { [] },
+    documentation => "Ignore lines exactly matching this.");
+has_option "ignore_substring" => (isa => "ArrayRef", cmd_aliases => "b",
+    "default" => sub { [] },
+    documentation => "Ignore lines containing this substring.");
+has_option "snippet" => (isa => "ArrayRef", cmd_aliases => "s",
+    "default" => sub { [] },
+    documentation => "Snip lines. Use s/search_re/replace/ syntax.");
 
 # Internal attributes (leading _ means not GetOpt).
-has_rw '_cmd' => (isa => 'ArrayRef', 'default' => sub { [] });
+has_rw "_cmd" => (isa => "ArrayRef", "default" => sub { [] });
 
-has_rw '_out' => (isa => 'ScalarRef[Str]', default => \&_strref);
-has_rw '_err' => (isa => 'ScalarRef[Str]', default => \&_strref);
+has_rw "_out" => (isa => "ScalarRef[Str]", default => \&_strref);
+has_rw "_err" => (isa => "ScalarRef[Str]", default => \&_strref);
 
-has_rw '_log' => (isa => 'FileHandle');
-has_rw '_term' => (isa => 'Term::ReadLine');
-has_rw '_snippet' => (isa => 'ArrayRef', 'default' => sub { [] });
+has_rw "_log" => (isa => "FileHandle");
+has_rw "_term" => (
+        isa => "Ref");
+        # TODO(gaal): figure out how to fix this:
+        # isa => "Term::ReadLine|Term::ReadLine::Perl|Term::ReadLine::Gnu");
+has_rw "_snippet" => (isa => "ArrayRef", "default" => sub { [] });
+has_rw "_ignore_re" => (isa => "ArrayRef", "default" => sub { [] });
 
-has_counter '_suppressed';
-has_counter '_output_lines';
+has_counter "_suppressed";
+has_counter "_output_lines";
 
 sub run {
     my($self) = @_;
@@ -80,6 +86,7 @@ sub run {
     }
 
     $self->open_log;
+    $self->import_re($_) for @{$self->ignore_re};
     $self->import_snippet($_) for @{$self->snippet};
 
     if ($self->need_pipe) {
@@ -107,15 +114,15 @@ sub open_log {
 
     my $now = $self->now_stamp;
     my $filename = $self->log;
-    $filename = ($self->need_pipe ? 'prolix.%d' : ($self->_cmd->[0] . '.%d')) if
-        $filename eq 'auto';
+    $filename = ($self->need_pipe ? "prolix.%d" : ($self->_cmd->[0] . ".%d")) if
+        $filename eq "auto";
     $filename = File::Spec->catfile(File::Spec->tmpdir, $filename) if
         $filename !~ m{[/\\]};  # Put in /tmp/ or similar unless we got a path.
     $filename =~ s/%d/$now/;  # TODO(gaal): implement incrementing %n.
 
     say "Logging output to $filename" if $self->verbose;
 
-    my $fh = IO::File->new($filename, 'w') or die "open: $filename: $!";
+    my $fh = IO::File->new($filename, "w") or die "open: $filename: $!";
     $self->_log($fh);
 }
 
@@ -130,7 +137,7 @@ sub now_stamp {
     my($self) = @_;
 
     my(@t) = localtime;  # Should this be gmtime?
-    return sprintf "%4d%02d%02dT%02d%02d",
+    return sprintf "%4d%02d%02dT%02d%02d%02d",
         $t[5] + 1900, $t[4] + 1, $t[3], $t[2], $t[1], $t[0];  # Ahh, UNIX.
 }
 
@@ -142,7 +149,7 @@ sub stats {
 
 # returns a fresh reference to a string.
 sub _strref {
-    return \(my $throwaway = '');
+    return \(my $throwaway = "");
 }
 
 sub run_pipe {
@@ -162,10 +169,10 @@ sub run_spawn {
         String::ShellQuote::shell_quote_best_effort(@{$self->_cmd})
         if $self->verbose;
 
-    Term::ReadKey::ReadMode('noecho');
-    END { Term::ReadKey::ReadMode('normal'); }
+    Term::ReadKey::ReadMode("noecho");
+    END { Term::ReadKey::ReadMode("normal"); }
 
-    $self->_term(Term::ReadLine->new('prolix'));
+    $self->_term(Term::ReadLine->new("prolix"));
     my $attribs = $self->_term->Attribs;
     $attribs->{completion_entry_function} =
         $attribs->{list_completion_function};
@@ -205,7 +212,7 @@ sub run_spawn {
     $ipc->finish;
     $self->consume_final;
 
-    Term::ReadKey::ReadMode('normal');
+    Term::ReadKey::ReadMode("normal");
 }
 
 sub _dump_stack {
@@ -220,49 +227,77 @@ sub try_user_input {
     # Enter interactive prompt mode. We hope this will be brief, and
     # IPC::Run can buffer our watched command in the meanhwile.
 
-    Term::ReadKey::ReadMode('normal');
+    Term::ReadKey::ReadMode("normal");
     while (my $cmd = $self->_term->readline("prolix>")) {
         $self->_term->addhistory($cmd);
         $self->handle_user_input($cmd);
     }
-    Term::ReadKey::ReadMode('restore');  # into noecho, we hope!
+    Term::ReadKey::ReadMode("restore");  # into noecho, we hope!
 }
 
 sub handle_user_input {
     my($self, $cmd) = @_;
-    given ($cmd) {
-        when (/^\s*stack\s*$/) { _dump_stack }
-        when (/^\s*bufs\s*$/) { $self->dump_bufs }
-        when (/^\s*q|quit\s*$/) { die "prolix-quit\n" }
-        when (/^\s*h|help\s*$/) { $self->help_interactive }
-        when (/^\s*pats\s*$/) { $self->dump_pats }
-        when (/^\s*stats\s*$/) { say $self->stats }
-        when (/^\s*(ignore_(?:line|re|substring))\s+(.*)/) {
-            my($ignore_type, $pat) = ($1, $2);
-            push @{ $self->$ignore_type }, $pat;
+    (my $nullary = $cmd) =~ s/^\s*(\S+)\s*/$1/;
+    if ($nullary) {
+        given ($nullary) {
+            when ("clear_all") { $self->clear_all }
+            when ("stack")     { _dump_stack }
+            when ("bufs")      { $self->dump_bufs }
+            when (/q|quit/)    { die "prolix-quit\n" }
+            when (/h|help/)    { $self->help_interactive }
+            when ("pats")      { $self->dump_pats }
+            when ("stats")     { say $self->stats }
+            default            { say "unknown command. try 'help'." }
         }
-        when (/^\s*snippet\s(.*)/) {
-            push @{ $self->snippet }, $1;
-            $self->import_snippet($1);
+    } else {
+        given ($cmd) {
+            when (/^\s*(ignore_(?:line|re|substring))\s+(.*)/) {
+                my($ignore_type, $pat) = ($1, $2);
+                push @{ $self->$ignore_type }, $pat;
+                $self->import_re($pat) if $ignore_type eq 're';
+            }
+            when (/^\s*snippet\s(.*)/) {
+                push @{ $self->snippet }, $1;
+                $self->import_snippet($1);
+            }
+            default { say "unknown command. try 'help'." }
         }
-        default { say "unknown command. try 'help'." }
     }
+}
+
+sub import_re {
+    my($self, $pat) = @_;
+
+    push @{ $self->_ignore_re }, qr/$pat/;
 }
 
 sub import_snippet {
     my($self, $snippet) = @_;
 
-    # $snippet =~ s/^s(.)(.*)(.)$/$2/ or die<<".";  # support flex delims?
-    # TODO(gaal): use Data::Munge's replace here to support backrefs.
-    my ($search, $replace) = $snippet =~ m,^s/(.*)/(.*)/$, or die<<".";
+    my $help = <<".";
 *** Usage: snippet s/find_re/replace/
 
-    Backreferences are not yet supported.
+    You may use Perl-like quoting on the substitution operation, so if your
+    pattern contains slashes use a different delimiter.
+
+    Modifiers that are honored: /igx   (m and s aren't meaningful here)
 .
+
+    my @extract = Text::Balanced::extract_quotelike($snippet);
+    my($op, $search, $replace, $modifiers) = @extract[3, 5, 8, 10];
+    die $help unless $op eq "s";
+    die $help unless defined $search;
+    die $help unless defined $replace;
+    my $mods = "";
+    for (qw/i x/) {
+        $mods .= $_ if $modifiers =~ /$_/;
+    }
+    my $global = $modifiers =~ /g/ ? "g" : "";
+    my $search_re = qr/(?$mods:$search)/;
+
     push @{ $self->_snippet }, sub {
         my($line) = @_;
-        $line =~ s/$search/$replace/;
-        return $line;
+        return Data::Munge::replace($line, $search_re, $replace, $global);
     };
 }
 
@@ -283,6 +318,7 @@ sub help_interactive {
     my($self) = @_;
 
     say <<"EOF";
+clear_all        - clear all patterns
 ignore_line      - add a full match to ignore
 ignore_re        - add an ignore pattern, e.g. ^(FINE|DEBUG)
 ignore_substring - add a partial match to ignore
@@ -293,6 +329,17 @@ snippet          - add a snippet expression, e.g. s/^(INFO|WARNING|ERROR) //
 
 To keep going, just enter an empty line.
 EOF
+}
+
+sub clear_all {
+    my($self) = @_;
+
+    @{ $self->ignore_line } = ();
+    @{ $self->ignore_re } = ();
+    @{ $self->_ignore_re } = ();
+    @{ $self->ignore_substring } = ();
+    @{ $self->snippet } = ();
+    @{ $self->_snippet } = ();
 }
 
 sub dump_bufs {
@@ -332,7 +379,7 @@ sub snip_line {
     return $line;
 }
 
-sub ok_line {
+sub process_line {
     my($self, $line) = @_;
 
     for my $exact (@{$self->ignore_line}) {
@@ -345,12 +392,12 @@ sub ok_line {
             return;
         }
     }
-    for my $pat (@{$self->ignore_re}) {
+    for my $pat (@{$self->_ignore_re}) {
         if ($line =~ $pat) {
             return;
         }
     }
-    return 1;
+    return $self->snip_line($line);
 }
 
 # One day, we might paint this in a different color or something.
@@ -360,9 +407,7 @@ sub on_out {
     my($self, $line) = @_;
     
     $self->inc__output_lines;
-    if ($self->ok_line($line)) {
-        $line = $self->snip_line($line);
-
+    if (defined($line = $self->process_line($line))) {
         say $line;
         if ($self->_log) {
             $self->_log->print("$line\n");
